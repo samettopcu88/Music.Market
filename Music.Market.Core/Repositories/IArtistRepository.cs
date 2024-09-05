cda901ce28073a97d6427796e53373a3fbf5ef45
OGLE <OGLE@DESKTOP-4QUP73B>
2024-09-05 14:15:00 +0300

    Core katmanı Models ve Repository.

---
+++ Music.Market.Core/Repositories/IArtistRepository.cs
@@ -0,0 +1,10 @@
+﻿using Music.Market.Core.Models;
+
+namespace Music.Market.Core.Repositories
+{
+    public interface IArtistRepository: IRepository<Artist>
+    {
+        Task<IEnumerable<Artist>> GetAllWithMusicsAsync();
+        Task<Artist> GetWithMusicsByIdAsync(int id);
+    }
+}
---
+++ Music.Market.Core/Repositories/IMusicRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Music.Market.Core.Repositories
+{
+    public interface IMusicRepository:IRepository<Models.Music>
+    {
+        Task<IEnumerable<Models.Music>> GetAllWithArtistAsync();
+        Task<Models.Music> GetWithArtistByIdAsync(int id);
+        Task<IEnumerable<Models.Music>> GetAllWithArtistByArtistIdAsync(int artistId);
+    }
+}
