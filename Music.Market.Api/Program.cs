01b62d93e38d068319041cae3ae084fd0235587b
OGLE <OGLE@DESKTOP-4QUP73B>
2024-09-05 16:49:57 +0300

    Son olarak migration başaryla eklendi.

--- Music.Market.Api/Program.cs
+++ Music.Market.Api/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using Music.Market.Core;
+using Music.Market.Data;
 
 namespace Music.Market.Api
 {
@@ -8,8 +11,15 @@ namespace Music.Market.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-
             builder.Services.AddControllers();
+            builder.Services.AddDbContext<MusicMarketDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
