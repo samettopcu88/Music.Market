f6dc0026b008ada95171bfde659cf438a1ed269a
OGLE <OGLE@DESKTOP-4QUP73B>
2024-09-05 16:28:21 +0300

    Data katmanında artist ve music repository ayarlandı.

---
+++ Music.Market.Data/Repositories/ArtistRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music.Market.Core.Models;
+using Music.Market.Core.Repositories;
+
+namespace Music.Market.Data.Repositories
+{
+    public class ArtistRepository : Repository<Artist>, IArtistRepository
+    {
+        public ArtistRepository(MusicMarketDbContext context) : base(context)
+        {
+
+        }
+
+        private MusicMarketDbContext MusicMarketDbContext
+        {
+            get { return Context as MusicMarketDbContext; }
+        }
+
+        public async Task<IEnumerable<Artist>> GetAllWithMusicsAsync()
+        {
+            return await MusicMarketDbContext.Artists
+                .Include(x => x.Musics)
+                .ToListAsync();
+        }
+
+        public async Task<Artist> GetWithMusicsByIdAsync(int id)
+        {
+            return await MusicMarketDbContext.Artists
+                .Include(x => x.Musics)
+                .SingleOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
---
+++ Music.Market.Data/Repositories/MusicRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Music.Market.Core.Repositories;
+
+namespace Music.Market.Data.Repositories
+{
+    public class MusicRepository : Repository<Core.Models.Music>, IMusicRepository
+    {
+        public MusicRepository(MusicMarketDbContext context) : base(context)
+        {
+
+        }
+
+        private MusicMarketDbContext MusicMarketDbContext
+        {
+            get { return Context as MusicMarketDbContext; }
+        }
+
+        public async Task<IEnumerable<Core.Models.Music>> GetAllWithArtistAsync()
+        {
+            return await MusicMarketDbContext.Musics
+                .Include(x => x.Artist)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Core.Models.Music>> GetAllWithArtistByArtistIdAsync(int artistID)
+        {
+            return await MusicMarketDbContext.Musics
+                .Include(x => x.Artist)
+                .Where(x => x.ArtistId == artistID)
+                .ToListAsync();
+        }
+
+        public async Task<Core.Models.Music> GetWithArtistByIdAsync(int id)
+        {
+            return await MusicMarketDbContext.Musics
+                .Include(x => x.Artist)
+                .SingleOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
